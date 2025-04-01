e597839b3798d9d1be2b5ece0f7ada58c5b4fc3c
21fatema <144434558+21fatema@users.noreply.github.com>
2025-04-01 07:23:17 +0100

    Update Program.cs

--- ShapeAreaCalculatorTask/Program.cs
+++ ShapeAreaCalculatorTask/Program.cs
@@ -10,10 +10,37 @@
             // Rectangle
             // Pentagon
             // Octagon
+
+            Console.WriteLine("Area of Circle: " + AreaOfCircle(5));
+            Console.WriteLine("Area of Triangle: " + AreaOfTriangle(10, 6));
+            Console.WriteLine("Area of Rectangle: " + AreaOfRectangle(8, 4));
+            Console.WriteLine("Area of Pentagon: " + AreaOfPentagon(7, 5));
+            Console.WriteLine("Area of Octagon: " + AreaOfOctagon(6));
         }
+        
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        
+        static double AreaOfTriangle(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+        
         static int areaOfRectangle()//Add the parameters
         {
+            return length * width;
+        }
+        
+        static double AreaOfPentagon(double side, double apothem)
+        {
+            return (5 * side * apothem) / 2;
+        }
 
+        static double AreaOfOctagon(double side)
+        {
+            return 2 * (1 + Math.Sqrt(2)) * Math.Pow(side, 2);
         }
     }
 }
